8f24c438bb11350428955a46c61f38011e3c8804
Patrik Schwartz <patosch007@gmail.com>
2019-10-07 09:13:30 +0200

    Oprava niektorych errorov, vytvorenie stranky pre admina, birmovancov

--- login.php
+++ login.php
@@ -12,11 +12,11 @@ if(isset($_SESSION["loginSuccess"])){
 
 ?>
 
-<html lang="en" dir="ltr">
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <title></title>
-        <PHP><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><PHP>
+        <!--PHP><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><PHP!-->
         <PHP><link rel="stylesheet" href="css/style.css"><PHP>
     </head>
     <body>
