8e4f7c1fcdfd90ff8ca4dc592b00a27c64c7db87
gizbyn <gizbyn@gmail.com>
2019-10-12 16:09:57 +0200

    Justifying

--- admin.php
+++ admin.php
@@ -26,55 +26,54 @@ $typeRows = $event_types->getEventTypes();
 ?>
 
 <html>
-	<head>
-		<!-- styling -->
-		<PHP><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></PHP>
-		<PHP><link rel="stylesheet" type="text/css" href="css/style.css"><PHP>
-	</head>
-	<body>
-		
-		<div class="container-fluid">
-			
-			<div>
-				<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-					<a class="navbar-brand" href="#">Domov</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse"
-					data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-						<div class="navbar-nav">
-							<a class="nav-item nav-link" href="#udalosti">Udalosti</a>
-							<a class="nav-item nav-link" href="#">Skupinky</a>
-							<a class="nav-item nav-link right-free" href="#">Rodičia</a>
-						</div>
-						<form class="form-inline my-2 my-lg-0"
-							action="core/forms/logout_handler.php"
-							method="post">
-							<input class="form-control mr-sm-2 btn-info"
-							type="submit"
-							aria-label="Odhlásiť sa"
-							name="logout"
-							value="Odhlásiť sa">
-						</form>
-					</div>
-				</nav>
-			</div>
-			
 
-			<!-- tu sa budu zobrazovat udalosti a moznost pridavania udalosti a editovanie ich -->
-			<div id="udalosti" style="padding: 20px; margin: 20px;">
-				<div class="container">
-					
-					<!-- NIC NEVIDIM CEZ TEN BLBY NAVBAR-->
-					<div style="width: 100%; height:  200px; background-color: blue;">
-						
-					</div>
+<head>
+    <!-- styling -->
+    <PHP>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </PHP>
+    <PHP>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </PHP>
+</head>
 
-					<!-- Tu budu zobrazene vsetky udalosti -->
-					<div>
-						
-					<?php
+<body>
+
+    <div class="container-fluid">
+
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <a class="navbar-brand" href="#">Domov</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="#udalosti">Udalosti</a>
+                        <a class="nav-item nav-link" href="#">Skupinky</a>
+                        <a class="nav-item nav-link right-free" href="#">Rodičia</a>
+                    </div>
+                    <form class="form-inline my-2 my-lg-0" action="core/forms/logout_handler.php" method="post">
+                        <input class="form-control mr-sm-2 btn-info" type="submit" aria-label="Odhlásiť sa" name="logout" value="Odhlásiť sa">
+                    </form>
+                </div>
+            </nav>
+        </div>
+
+
+        <!-- tu sa budu zobrazovat udalosti a moznost pridavania udalosti a editovanie ich -->
+        <div id="udalosti" style="padding: 20px; margin: 20px;">
+            <div class="container">
+
+                <!-- NIC NEVIDIM CEZ TEN BLBY NAVBAR-->
+                <div style="width: 100%; height:  200px; background-color: blue;">
+
+                </div>
+
+                <!-- Tu budu zobrazene vsetky udalosti -->
+                <div>
+
+                    <?php
 
 						foreach ($eventsRows as $eventRow) {
 							printEventPost($eventRow);
@@ -82,55 +81,55 @@ $typeRows = $event_types->getEventTypes();
 
 					?>
 
-					</div>
+                </div>
 
-					<!-- Formular pre pridavanie udalosti -->
-					<div>						
-						<form action='core/forms/admin_form_handler.php' method='post'>
-							<label class='row'>
-								<input class='input' type='hidden' name='event_id' value=''>
-							</label>
-							<label class='row'>Názov udalosti
-								<input class='input' type='text' name='event_name' value='' placeholder='Názov udalosti' required>
-							</label>
-							<label class='row'>Opis udalosti
-								<textarea class='input' rows='4' cols='50' type='text' name='event_description' value='' placeholder='Opis udalosti'></textarea>
-							</label>
-							<label for="row">Typ udalosti
-								<select name='event_type'>
-									<?php
+                <!-- Formular pre pridavanie udalosti -->
+                <div>
+                    <form action='core/forms/admin_form_handler.php' method='post'>
+                        <label class='row'>
+                            <input class='input' type='hidden' name='event_id' value=''>
+                        </label>
+                        <label class='row'>Názov udalosti
+                            <input class='input' type='text' name='event_name' value='' placeholder='Názov udalosti' required>
+                        </label>
+                        <label class='row'>Opis udalosti
+                            <textarea class='input' rows='4' cols='50' type='text' name='event_description' value='' placeholder='Opis udalosti'></textarea>
+                        </label>
+                        <label for="row">Typ udalosti
+                            <select name='event_type'>
+                                <?php
 										foreach ($typeRows as $typeRow) {
 											echo "<option value='$typeRow[0]'> $typeRow[1]</option>";
 										}
 									?>
-								</select>
-							</label>
-							<label for="row">Dátum začiatku
-								<input class='input' type="date" name="date_from" value="" placeholder="Dátum začiatku">
-							</label>
-							<label for="row">Dátum konca
-								<input class='input' type="date" name="date_to" value="" placeholder="Dátum konca">
-							</label>
-							<label for="row">Čas začiatku
-								<input class='input' type="time" name="time_from" value="" placeholder="Čas začiatku">
-							</label>
-							<label for="row">Čas konca
-								<input class='input' type="time" name="time_to" value="" placeholder="Čas konca">
-							</label>
-							<label class='row'>Miesto udalosti
-								<input class='input' type='text' name='event_place' value='' placeholder='Miesto udalosti'>
-							</label>
-							<label class='row'>
-								<input class='input btn btn-info' type='submit' name='submit' value='Pridať udalosť'>
-							</label>
-						</form>
-					</div>
+                            </select>
+                        </label>
+                        <label for="row">Dátum začiatku
+                            <input class='input' type="date" name="date_from" value="" placeholder="Dátum začiatku">
+                        </label>
+                        <label for="row">Dátum konca
+                            <input class='input' type="date" name="date_to" value="" placeholder="Dátum konca">
+                        </label>
+                        <label for="row">Čas začiatku
+                            <input class='input' type="time" name="time_from" value="" placeholder="Čas začiatku">
+                        </label>
+                        <label for="row">Čas konca
+                            <input class='input' type="time" name="time_to" value="" placeholder="Čas konca">
+                        </label>
+                        <label class='row'>Miesto udalosti
+                            <input class='input' type='text' name='event_place' value='' placeholder='Miesto udalosti'>
+                        </label>
+                        <label class='row'>
+                            <input class='input btn btn-info' type='submit' name='submit' value='Pridať udalosť'>
+                        </label>
+                    </form>
+                </div>
 
-					<!-- Formular pre vymazanie udalosti -->
-					<div>
-						<form action="core/forms/admin_form_handler.php" method="post">
+                <!-- Formular pre vymazanie udalosti -->
+                <div>
+                    <form action="core/forms/admin_form_handler.php" method="post">
 
-							<?php
+                        <?php
 							foreach ($eventsRows as $eventRow) {
 								echo "<label class='row'>";
 									echo"<input class='input' type='checkbox' name='eventId[]' value='$eventRow[0]'> $eventRow[1]";
@@ -138,22 +137,29 @@ $typeRows = $event_types->getEventTypes();
 							}
 							?>
 
-							<label class='row'>
-								<input class='input btn btn-info' type='submit' name='submit' value='Vymazať udalosť'>
-							</label>
-						</form>
-					</div>
-					
-				</div>
-			</div>
-			<div>
-				
-			</div>
-		</div>
-		<PHP><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script><PHP>
-		<PHP><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script><PHP>
-		<PHP><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script><PHP>
-	</body>
+                        <label class='row'>
+                            <input class='input btn btn-info' type='submit' name='submit' value='Vymazať udalosť'>
+                        </label>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+    <PHP>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    </PHP>
+    <PHP>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    </PHP>
+    <PHP>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </PHP>
+</body>
+
 </html>
 
 <!-- ////////////// -->
--- login.php
+++ login.php
@@ -16,15 +16,21 @@ if(isset($_SESSION["loginSuccess"])){
 ?>
 
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <PHP><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><PHP>
-        <PHP><link rel="stylesheet" href="css/style.css"><PHP>
-    </head>
-    <body>
 
-        <?php
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <PHP>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </PHP>
+    <PHP>
+        <link rel="stylesheet" href="css/style.css">
+    </PHP>
+</head>
+
+<body>
+
+    <?php
 
         // if login was unsuccesfull, user will see note about it
         if(!$loginSuccess){
@@ -36,21 +42,22 @@ if(isset($_SESSION["loginSuccess"])){
 
         ?>
 
-        <!-- form for login -->
-        <div class="container form-container">
-            <div class="form">
-                <form action="core/forms/login_handler.php?t=<?php echo"$time"?>" method="post">
-                    <label class="row" for="">
-                        <input type="text" name="login" value="" placeholder="Login" required>
-                    </label>
-                    <label class="row" for="">
-                        <input type="password" name="password" value="" placeholder="Heslo" required>
-                    </label>
-                    <label class="row" for="">
-                        <input  class="login-btn btn btn-info" type="submit" name="submit" value="Prihlásiť sa">
-                    </label>
-                </form>
-            </div>
+    <!-- form for login -->
+    <div class="container form-container">
+        <div class="form">
+            <form action="core/forms/login_handler.php?t=<?php echo"$time"?>" method="post">
+                <label class="row" for="">
+                    <input type="text" name="login" value="" placeholder="Login" required>
+                </label>
+                <label class="row" for="">
+                    <input type="password" name="password" value="" placeholder="Heslo" required>
+                </label>
+                <label class="row" for="">
+                    <input class="login-btn btn btn-info" type="submit" name="submit" value="Prihlásiť sa">
+                </label>
+            </form>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
