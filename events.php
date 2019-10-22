d7f040e98704afc34e1c8bc696b19adb1bbcfe8b
gizbyn <gizbyn@gmail.com>
2019-10-22 15:55:13 +0200

    backgorund

--- events.php
+++ events.php
@@ -44,16 +44,14 @@ $allUsersRows = $users->getAllUsers();
     <PHP>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </PHP>
-    <PHP>
-        <link rel="stylesheet" type="text/css" href="/css/style.css">
-    </PHP>
+
     <PHP>
         <link rel="stylesheet" type="text/css" href="/css/style-events.css">
     </PHP>
 </head>
 
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid header">
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <a class="navbar-brand" href="domov">Domov</a>
