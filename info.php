bb0ac9ac2e68014d7254e7e1aafc0ab28f89e282
Patrik Schwartz <patosch007@gmail.com>
2019-10-28 15:03:04 +0100

    pdf

--- info.php
+++ info.php
@@ -80,7 +80,7 @@ include_once("core/init.inc.php");
         </div>
 
         <h2 class="title-pages questions">Otázky na birmovaneckú skúšku</h2>
-        <object data="Birmovka_otázky_a_odpovede_POPRAD.pdf" type="application/pdf" width="60%" height="100%" class="justify-content-center container-fluid otazky-a-odpovede">
+        <object data="birmovka.pdf" type="application/pdf" width="60%" height="100%" class="justify-content-center container-fluid otazky-a-odpovede">
             <p>Pravdepodobne Váš prehliadač nepodporuje zobrazovanie PDF súborov. Nevadí. <a class="not-showing-pdf" href="birmovka.pdf">Kliknite tu a stiahnite si tento PDF súbor</a>.</p>
         </object>
     </main>
@@ -98,16 +98,10 @@ include_once("core/init.inc.php");
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     </PHP>
     <PHP>
-        <script src="js/inputLabelChanger.js"></script>
-    </PHP>
-    <PHP>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     </PHP>
     <PHP>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </PHP>
-    <PHP>
-        <script src="js/pdfViewer.js"></script>
     </PHP>
 </body>
 
